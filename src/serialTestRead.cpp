8369d56a99f7f4630a47b1ac67b6e56a2a234c5d
TrippleBender <bennide03@gmx.de>
2018-09-18 13:27:53 +0200

    erhält ab und zu die richtigen Daten

--- src/serialTestRead.cpp
+++ src/serialTestRead.cpp
@@ -6,6 +6,8 @@
  */
 
 #include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
 #include <sstream>
 #include <fstream>
 #include <cstdlib>
@@ -20,38 +22,50 @@ int main (int argc, char** argv)
 	my_serial_stream.Open("/dev/ttyUSB0");
 
 	//Set the baudrate
-	my_serial_stream.SetBaudRate(LibSerial::SerialStreamBuf::BAUD_9600);
+	my_serial_stream.SetBaudRate(LibSerial::BaudRate::BAUD_9600);
 
 	//Setting Character Size
-	my_serial_stream.SetCharSize(LibSerial::SerialStreamBuf::CHAR_SIZE_8);
+	my_serial_stream.SetCharacterSize(LibSerial::CharacterSize::CHAR_SIZE_8);
 
 	//Setting Number of Stop Bits
-	my_serial_stream.SetNumOfStopBits(1);
+	my_serial_stream.SetStopBits(LibSerial::StopBits::STOP_BITS_1);
 
 	//Setting the Parity Type
-	my_serial_stream.SetParity(LibSerial::SerialStreamBuf::PARITY_NONE);
+	my_serial_stream.SetParity(LibSerial::Parity::PARITY_NONE);
 
 	//Setting the Flow-Control Type
-	my_serial_stream.SetFlowControl(LibSerial::SerialStreamBuf::FLOW_CONTROL_NONE);
+	my_serial_stream.SetFlowControl(LibSerial::FlowControl::FLOW_CONTROL_NONE);
 
 	std::cout << "Port eingerichtet!" << std::endl;
-	usleep(200000);
+
+	// Wait for data to be available at the serial port.
+	while(my_serial_stream.rdbuf()->in_avail() == 0)
+	{
+		usleep(1000);
+	}
 
 	//Read something!
-	char inString;
+
+	std::string inString = "";
+	const char *str = NULL;
 
 	int data = 0;
 
 	std::cout << "Daten empfangen!" << std::endl;
 
-	while(1)
+	while(my_serial_stream.IsDataAvailable())
 	{
-		my_serial_stream >> inString;
+		//Read
+		//my_serial_stream >> inChar;
+		std::getline(my_serial_stream, inString);
+		/*str = inString;
+		data = atoi(str);*/
 
 		//data = atoi(&inString);
 		//std::cout << data << std::endl;						//funktioniert auch mit serial.write unter Arduino
 
 		std::cout << inString << std::endl;					//funktioniert nur mit serial.print unter Arduino
+		//std::cout << data << std::endl;
 		std::cout << "---" << std::endl;
 	}
 }
